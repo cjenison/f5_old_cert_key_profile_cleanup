a8b47a5f61f7b70f33afbfeb869b4ce061b57add
Chad Jenison <c.jenison@f5.com>
2018-07-18 13:29:48 -0500

    merged in code to do token-based auth for iControl REST

--- f5_old_cert_key_profile_cleanup.py
+++ f5_old_cert_key_profile_cleanup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 # f5_old_cert_key_profile_cleanup.py
 # Author: Chad Jenison (c.jenison at f5.com)
 # Version 1.0
@@ -19,18 +19,48 @@ parser.add_argument('--user', help='username to use for authentication', require
 args = parser.parse_args()
 
 
+
+def get_auth_token(bigip, username, password):
+    authbip = requests.session()
+    authbip.verify = False
+    payload = {}
+    payload['username'] = username
+    payload['password'] = password
+    payload['loginProviderName'] = 'tmos'
+    authurl = 'https://%s/mgmt/shared/authn/login' % (bigip)
+    authPost = authbip.post(authurl, headers=contentJsonHeader, data=json.dumps(payload))
+    if authPost.status_code == 404:
+        print ('attempt to obtain authentication token failed; will fall back to basic authentication; remote LDAP auth will require configuration of local user account')
+        token = None
+    elif authPost.status_code == 401:
+        print ('attempt to obtain authentication token failed due to invalid credentials')
+        token = 'Fail'
+    elif authPost.json().get('token'):
+        token = authPost.json()['token']['token']
+        print ('Got Auth Token: %s' % (token))
+    else:
+        print ('Unexpected error attempting POST to get auth token')
+        quit()
+    return token
+
 user = args.user
 passwd = getpass.getpass("Password for " + user + ":")
 bip = requests.session()
-bip.auth = (user, passwd)
+token = get_auth_token(args.bigip, args.user, password)
+if token and token != 'Fail':
+    bip.headers.update({'X-F5-Auth-Token': token})
+else:
+    bip.auth = (username, password)
 bip.verify = False
 requests.packages.urllib3.disable_warnings()
 url_base = ('https://%s/mgmt/tm' % (args.bigip))
 
+
+
 clientsslprofileset = set()
 clientsslprofiles = bip.get('%s/ltm/profile/client-ssl' % (url_base)).json()
 for clientssl in clientsslprofiles['items']:
-    #print ('Client SSL Profile: %s - Cert: %s' % (clientssl['name'], clientssl['cert'])) 
+    #print ('Client SSL Profile: %s - Cert: %s' % (clientssl['name'], clientssl['cert']))
     clientsslprofileset.add(clientssl['name'])
 
 
@@ -51,5 +81,3 @@ for virtual in virtuals['items']:
             print('Cert: %s' % (clientsslprofile['cert']))
             print('Cert Expiration: %s' % (certinfo['expirationString']))
             print('Cert Subject: %s' % (certinfo['subject']))
-
-
