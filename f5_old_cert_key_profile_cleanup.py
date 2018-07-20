8d7c658698730f12964cf9f03ae4de742c8ef389
Chad Jenison <c.jenison@f5.com>
2018-07-19 19:46:53 -0500

    sets for used profiels,certs and keys; will add corresponding "unused"

--- f5_old_cert_key_profile_cleanup.py
+++ f5_old_cert_key_profile_cleanup.py
@@ -17,6 +17,7 @@ parser.add_argument('--bigip', help='IP or hostname of BIG-IP Management or Self
 parser.add_argument('--user', help='username to use for authentication', required=True)
 
 args = parser.parse_args()
+contentJsonHeader = {'Content-Type': "application/json"}
 
 
 
@@ -44,7 +45,7 @@ def get_auth_token(bigip, username, password):
     return token
 
 user = args.user
-passwd = getpass.getpass("Password for " + user + ":")
+password = getpass.getpass("Password for " + user + ":")
 bip = requests.session()
 token = get_auth_token(args.bigip, args.user, password)
 if token and token != 'Fail':
@@ -58,6 +59,11 @@ url_base = ('https://%s/mgmt/tm' % (args.bigip))
 
 
 clientsslprofileset = set()
+usedclientsslprofileset = set()
+certset = set()
+keyset = set()
+usedcertset = set()
+usedkeyset = set()
 clientsslprofiles = bip.get('%s/ltm/profile/client-ssl' % (url_base)).json()
 for clientssl in clientsslprofiles['items']:
     #print ('Client SSL Profile: %s - Cert: %s' % (clientssl['name'], clientssl['cert']))
@@ -68,16 +74,20 @@ virtuals = bip.get('%s/ltm/virtual' % (url_base)).json()
 for virtual in virtuals['items']:
 #    print ('Virtual: %s' % (virtual['name']))
     virtualprofiles = bip.get('%s/ltm/virtual/%s/profiles' % (url_base, virtual['name'])).json()
-    for profile in virtualprofiles['items']:
-	if (profile['name'] in clientsslprofileset):
-	    clientsslprofile = bip.get('%s/ltm/profile/client-ssl/%s' % (url_base, profile['name'])).json()
-            certurlfragment = clientsslprofile['cert'].replace("/", "~", 2)
-            print('Virtual: %s' % (virtual['name']))
-            print('Virtual Destination: %s' % (virtual['destination']))
-            if 'description' in virtual:
-                print('Virtual Description: %s' % (virtual['description']))
-            certinfo = bip.get('%s/sys/file/ssl-cert/%s' % (url_base, certurlfragment)).json()
-	    print('SSL Profile: %s' % (profile['name']))
-            print('Cert: %s' % (clientsslprofile['cert']))
-            print('Cert Expiration: %s' % (certinfo['expirationString']))
-            print('Cert Subject: %s' % (certinfo['subject']))
+    if virtualprofiles.get('items'):
+        for profile in virtualprofiles['items']:
+            if profile['name'] in clientsslprofileset:
+                usedclientsslprofileset.add(profile['name'])
+                clientsslprofile = bip.get('%s/ltm/profile/client-ssl/%s' % (url_base, profile['name'])).json()
+                usedcertset.add(clientsslprofile['cert'])
+                usedkeyset.add(clientsslprofile['key'])
+                certurlfragment = clientsslprofile['cert'].replace("/", "~", 2)
+                print('Virtual: %s' % (virtual['name']))
+                print('Virtual Destination: %s' % (virtual['destination']))
+                if 'description' in virtual:
+                    print('Virtual Description: %s' % (virtual['description']))
+                certinfo = bip.get('%s/sys/file/ssl-cert/%s' % (url_base, certurlfragment)).json()
+                print('SSL Profile: %s' % (profile['name']))
+                print('Cert: %s' % (clientsslprofile['cert']))
+                print('Cert Expiration: %s' % (certinfo['expirationString']))
+                print('Cert Subject: %s' % (certinfo['subject']))
